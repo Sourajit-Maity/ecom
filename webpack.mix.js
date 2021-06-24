d99c3de0d3e5ae7cafa88e686618f322263518dd
Atanu <webdevwdc@gmail.com>
2021-06-24 20:41:55 +0530

    output path is removed

--- webpack.mix.js
+++ webpack.mix.js
@@ -28,8 +28,5 @@ mix.webpackConfig({
             alias: {
                 '@': path.resolve(__dirname, 'resources/js/')
             }
-        },
-        output: {
-            path: path.resolve(__dirname, 'public/js/')
         }
     });
