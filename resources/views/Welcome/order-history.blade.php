7b17472ce6613c93674039e27ebc244e7668bbdb
Sourajit-Maity <you@example.com>
2021-10-07 20:50:00 +0530

    orderdetailsinorderhistory

--- resources/views/Welcome/order-history.blade.php
+++ resources/views/Welcome/order-history.blade.php
@@ -11,8 +11,10 @@
                         <tr>
                             <th>date</th>
                             <th>Order ID</th>
+                            <th>Names</th>
                             <th>status</th>
                             <th>Product</th>
+                            <th>Quantity</th>
                             <th>Total</th>
                         </tr>
                     </thead>
@@ -23,6 +25,7 @@
         
                         <td>{!! \Carbon\Carbon::parse($order->created_at)->format('d M Y') !!}</td>
                         <td>##{{ $order->id }}</td>
+                        <td>{{ $order->names }}</td>
                         <td>{{ $order->status }}</td>
                         <td>
                             <div class="image-wraper">
@@ -32,8 +35,44 @@
                                 </div>
                             </div>
                         </td>
+                        <td>{{ $order->quantity }}</td>
                         <td>
                             $ {{ $order->payment_price }}
+                        </td>
+                    </tr>
+                    @endforeach
+                        @else
+                            <tr>
+                                <td colspan="7">No entries in table</td>
+                            </tr>
+                        @endif
+                        @if (count($orderdetails) > 0)
+                            @foreach ($orderdetails as $orderdetail)
+                    <tr>
+        
+                        <td>{!! \Carbon\Carbon::parse($orderdetail->created_at)->format('d M Y') !!}</td>
+                        <td>##{{ $orderdetail->order_id }}</td>
+                        <td>{{ $orderdetail->name }}</td>
+                        @if( $orderdetail->status == 1 )
+                        <td>Processing</td>
+                        @elseif( $orderdetail->status == 2 )
+                        <td>Accepted</td>
+                        @elseif( $orderdetail->status == 4 )
+                        <td>Delivered</td>
+                        @else
+                        <td>Cancel</td>
+                        @endif
+                        <td>
+                            <div class="image-wraper">
+                                <div class="order-image">
+                                    <img src="{{ $orderdetail->image }}" alt="">
+                                    <a href="#url"><img src="{{asset('welcome_assets/images/close.svg')}}" alt=""></a>
+                                </div>
+                            </div>
+                        </td>
+                        <td>{{ $orderdetail->quantity }}</td>
+                        <td>
+                            $ {{ $orderdetail->price }}
                         </td>
                     </tr>
                     @endforeach
